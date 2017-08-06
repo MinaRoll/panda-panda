84bad6a83d465fd25c05b72d7c2c4a163ac16780
romina <romina.torres.b@gmail.com>
2017-08-06 00:22:32 -0400

    agrego funcionalidad para mostrar imagenes en JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,11 @@ document.getElementById("cerrar3").addEventListener("click",function(){
 document.getElementById("cerrar4").addEventListener("click",function(){
 	document.getElementById("photo4").classList.toggle("ocultar");
 })
+
+//Muestra las imagenes al hacer click en 'restaurar'
+document.getElementById("restaurar").addEventListener("click",function(){
+document.getElementById("photo1").classList.remove("ocultar");
+document.getElementById("photo2").classList.remove("ocultar");
+document.getElementById("photo3").classList.remove("ocultar");
+document.getElementById("photo4").classList.remove("ocultar");
+})
